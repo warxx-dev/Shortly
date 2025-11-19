cecacdb83eb1803daecc42c09d6f862815770dde
Walter <waltercarraz04@gmail.com>
2025-11-19 17:12:51 -0500

    preparing front

--- API/src/main.ts
+++ API/src/main.ts
@@ -15,18 +15,9 @@ async function bootstrap() {
   app.enableCors({
     origin: process.env.CLIENT_URL,
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
   app.use(cookieParser());
-
-  // // Headers for Google OAuth
-  // app.use((req, res, next) => {
-  //   res.setHeader('Cross-Origin-Opener-Policy', 'same-origin-allow-popups');
-  //   res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
-  //   next();
-  // });
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
